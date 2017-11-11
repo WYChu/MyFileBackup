b8df6448fa5a527a35c23b1a0803645ebfdf71a5
朱維義 <will.chu@senao.com.tw>
2017-11-11 21:02:43 +0800

    Homework 4

---
+++ MyFileBackup/Factories/FileFinderFactory.cs
@@ -0,0 +1,32 @@
+﻿using MyFileBackup.Handlers;
+using MyFileBackup.Interfaces;
+using MyFileBackup.Medels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFileBackup.Factories
+{
+    public class FileFinderFactory
+    {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="finder"></param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static IFileFinder Create(string finder, Config config)
+        {
+            if (finder == "file")
+            {
+                return new LocalFileFinder(config);
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ MyFileBackup/Handlers/AbstractFileFinder.cs
@@ -0,0 +1,57 @@
+﻿using MyFileBackup.Interfaces;
+using MyFileBackup.Medels;
+using MyFileBackup.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFileBackup.Handlers
+{
+    /// <summary>
+    ///
+    /// </summary>
+    public abstract class AbstractFileFinder : IFileFinder
+    {
+        protected Config config;
+        protected string[] files;
+        protected int index = -1;
+
+        protected AbstractFileFinder()
+        {
+        }
+
+        protected AbstractFileFinder(Config config)
+        {
+            if (config != null)
+                this.config = config;
+        }
+
+        // IEnumerator
+        public object Current =>
+        this.CreateCandidate(this.files[this.index]);
+
+        // IEnumerable
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator)this;
+        }
+
+        // IEnumerator
+        public bool MoveNext()
+        {
+            this.index++;
+            return (this.index < this.files.Count());
+        }
+
+        // IEnumerator
+        public void Reset()
+        {
+            this.index = -1;
+        }
+
+        protected abstract Candidate CreateCandidate(string fileName);
+    }
+}
